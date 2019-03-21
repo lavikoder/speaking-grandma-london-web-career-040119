866523144ad614925e622c15c868c87a57fee90d
Kevin Kamanda <kevkamanda@gmail.com>
2019-03-21 18:30:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma(phrase)
   
-  if phrase = "I LOVE YOU GRANDMA!"
+  if phrase = 'I LOVE YOU GRANDMA!'
     puts "I LOVE YOU TOO PUMPKIN!"
     
   elsif phrase = phrase.uppercase
@@ -13,4 +13,4 @@ end
 
 
 
-speak_to_grandma('Hi!')
+speak_to_grandma('I LOVE YOU GRANDMA!')
