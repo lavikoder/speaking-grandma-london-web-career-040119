391390607d3d4d2a8d378ca37ba3078f5041efb7
Kevin Kamanda <kevkamanda@gmail.com>
2019-03-21 18:27:53 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(phrase)
   
-  if phrase == "I LOVE YOU GRANDMA!"
+  if phrase = "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
     
-  elsif phrase == phrase.uppercase
+  elsif phrase = phrase.uppercase
     puts "NO, NOT SINCE 1938!"
     
   else
